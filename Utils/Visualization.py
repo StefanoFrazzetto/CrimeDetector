29346483b516f9a611a35fdd2424cc03fa2a7911
Stefano Frazzetto <s.frazzetto22@gmail.com>
2018-11-19 04:27:46 +0000

    Add static method decorator

--- Utils/Visualization.py
+++ Utils/Visualization.py
@@ -4,5 +4,6 @@ import seaborn as sns;
 class Visualization(object):
     sns.set(font_scale=1.2)
 
+    @staticmethod
     def plot(label1, label2, data):
         sns.lmplot(label1, label2, data=data, hue='Type', palette='Set1', fit_reg=False, scatter_kws={"s": 70})
