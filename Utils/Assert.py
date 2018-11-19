37c73b252f5555578d0b97238e7b16aad91925e3
Stefano Frazzetto <s.frazzetto22@gmail.com>
2018-11-19 03:53:46 +0000

    Add method to check if var is True

--- Utils/Assert.py
+++ Utils/Assert.py
@@ -1,12 +1,19 @@
+from typing import Any
+
+
 class Assert:
     @staticmethod
     def not_empty(obj: list, message: str = None):
         assert len(obj) > 0, "The list is empty." if message is None else message
 
     @staticmethod
-    def not_none(obj, message: str = None):
+    def not_none(obj: Any, message: str = None):
         assert obj is not None, "The object value is None." if message is None else message
 
     @staticmethod
-    def same_length(obj1, obj2, message: str = None):
-        assert len(obj1) == len(obj2), "The objects have different lengths" if message is None else message
+    def same_length(obj1: Any, obj2: Any, message: str = None):
+        assert len(obj1) == len(obj2), "The objects have different lengths." if message is None else message
+
+    @staticmethod
+    def true(var: Any, message: str = None):
+        assert var is True, "The variable is not true." if message is None else message
