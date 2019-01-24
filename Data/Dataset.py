70184e3cf0de5fb5d901159b7ad423f4b5cb4860
Stefano Frazzetto <s.frazzetto22@gmail.com>
2019-01-24 18:00:43 +0000

    Replace variables named testing with validation

--- Data/Dataset.py
+++ Data/Dataset.py
@@ -3,6 +3,7 @@ from enum import Enum
 from typing import List
 
 import pandas as pd
+import numpy as np
 
 from Interfaces import Serializable, Analyzable
 from Utils import Log, Numbers
@@ -10,7 +11,7 @@ from Utils import Log, Numbers
 
 class DatasetCategory(Enum):
     TRAINING = 0
-    TESTING = 1
+    VALIDATION = 1
 
 
 class Dataset(Serializable):
@@ -18,27 +19,24 @@ class Dataset(Serializable):
     Category-agnostic dataset.
 
     The put operation automatically puts the data into either
-    training or testing according to the defined split ratio.
+    training or validation according to the defined split ratio.
     """
     training: pd.DataFrame
-    testing: pd.DataFrame
+    validation: pd.DataFrame
 
     __training: List[Analyzable]
-    __testing: List[Analyzable]
+    __validation: List[Analyzable]
 
     def __init__(self, split_ratio=0.7, max_data=math.inf, language='english'):
         self.split_ratio = split_ratio
         self.max_data = max_data
         self.language = language
 
-        self.negative = 0
-        self.positive = 0
-
         self.__training = []
-        self.__testing = []
+        self.__validation = []
 
         self.training = None
-        self.testing = None
+        self.validation = None
 
         self.finalized = False
 
@@ -49,20 +47,20 @@ class Dataset(Serializable):
     def __add_to_training(self, data: Analyzable):
         self.__training.append(data.to_dictionary())
 
-    def __add_to_testing(self, data: Analyzable):
-        self.__testing.append(data.to_dictionary())
+    def __add_to_validation(self, data: Analyzable):
+        self.__validation.append(data.to_dictionary())
 
     def __get_training_size(self):
         return len(self.__training)
 
-    def __get_testing_size(self):
-        return len(self.__testing)
+    def __get_validation_size(self):
+        return len(self.__validation)
 
     def __get_total_size(self):
-        return self.__get_training_size() + self.__get_testing_size()
+        return self.__get_training_size() + self.__get_validation_size()
 
     def __get_current_split_ratio(self):
-        if self.__get_training_size() == 0 or self.__get_testing_size() == 0:
+        if self.__get_training_size() == 0 or self.__get_validation_size() == 0:
             return 0
 
         return self.__get_training_size() / self.__get_total_size()
@@ -74,24 +72,24 @@ class Dataset(Serializable):
     def get_training_size(self):
         return len(self.training)
 
-    def get_testing_size(self):
-        return len(self.testing)
+    def get_validation_size(self):
+        return len(self.validation)
 
     def get_total_size(self):
-        return self.get_training_size() + self.get_testing_size()
+        return self.get_training_size() + self.get_validation_size()
 
     def get_split_ratio(self):
-        if self.get_training_size() == 0 or self.get_testing_size() == 0:
+        if self.get_training_size() == 0 or self.get_validation_size() == 0:
             return 0
         return self.get_training_size() / self.get_total_size()
 
     def finalize(self):
         self.finalized = True
         self.training = pd.DataFrame(self.__training)
-        self.testing = pd.DataFrame(self.__testing)
+        self.validation = pd.DataFrame(self.__validation)
 
         self.__training = None
-        self.__testing = None
+        self.__validation = None
 
     def put(self, data: Analyzable, data_category: DatasetCategory = None):
         """
@@ -107,67 +105,64 @@ class Dataset(Serializable):
         if self.__get_total_size() >= self.max_data:
             return
 
-        if data.is_negative():
-            self.negative += 1
-        else:
-            self.positive += 1
-
         # Automatically split sets using split ratio
         if data_category is None:
             if self.__get_training_size() == 0:
                 self.__add_to_training(data)
-            elif self.__get_testing_size() == 0:
-                self.__add_to_testing(data)
+            elif self.__get_validation_size() == 0:
+                self.__add_to_validation(data)
             else:
                 self.__add_to_training(data) \
                     if self.__get_current_split_ratio() <= self.split_ratio \
-                    else self.__add_to_testing(data)
+                    else self.__add_to_validation(data)
 
         # Manually assign data
         elif data_category == DatasetCategory.TRAINING:
             self.__add_to_training(data)
-        elif data_category == DatasetCategory.TESTING:
-            self.__add_to_testing(data)
+        elif data_category == DatasetCategory.VALIDATION:
+            self.__add_to_validation(data)
 
-    def get_positives(self):
-        return (self.training['label'] == 1).sum()
+    @staticmethod
+    def get_positives(dataset):
+        return (dataset['label'] == 1).sum()
 
-    def get_negatives(self):
-        return (self.training['label'] == 0).sum()
+    @staticmethod
+    def get_negatives(dataset):
+        return (dataset['label'] == 0).sum()
 
     def log_info(self):
         Log.info(f"### DATASET SAMPLES ###", header=True)
-        Log.info(f"Total: {self.get_training_size() + self.get_testing_size()} - "
+        Log.info(f"Total: {self.get_training_size() + self.get_validation_size()} - "
                  f"Training: {self.get_training_size()} / "
-                 f"Testing: {self.get_testing_size()}.")
+                 f"Validation: {self.get_validation_size()}.")
 
-        Log.info(f"Positive (P): {self.get_positives()} / "
-                 f"Negative (N): {self.get_negatives()} - "
-                 f"Ratio (P/Total): {Numbers.get_formatted_percentage(self.get_positives(), self.get_positives() + self.get_negatives())} %")
+        Log.info("# TRAINING", header=True)
+        ratio = Numbers.get_formatted_percentage(
+            self.get_positives(self.training), self.get_positives(self.training) + self.get_negatives(self.training))
+        Log.info(f"Positive (P): {self.get_positives(self.training)} / "
+                 f"Negative (N): {self.get_negatives(self.training)} - "
+                 f"Ratio (P/Total): {ratio} %")
+
+        Log.info("# VALIDATION", header=True)
+        ratio = Numbers.get_formatted_percentage(
+            self.get_positives(self.validation),
+            self.get_positives(self.validation) + self.get_negatives(self.validation))
+        Log.info(f"Positive (P): {self.get_positives(self.validation)} / "
+                 f"Negative (N): {self.get_negatives(self.validation)} - "
+                 f"Ratio (P/Total): {ratio} %")
 
         split_ratio = Numbers.get_formatted_percentage(self.get_training_size(), self.get_total_size())
         Log.info(f"Dataset split ratio: {split_ratio} %")
 
-    def under_sample(self, ratio=0.5):
-        self.training = self.training.drop(self.training.query('label == 0').sample(frac=ratio).index)
+    def balance_negatives(self):
+        training_positives = self.get_positives(self.training)
+        training_negatives = self.get_negatives(self.training)
+        training_frac = 1 - (training_positives / training_negatives)
+        self.training = self.training.drop(self.training.query('label == 0')
+                                           .sample(frac=training_frac, random_state=42).index)
 
-    # def under_sample(self):
-    #     positives = self.positive
-    #     count = 0
-    #     temp_dataset = []
-    #
-    #     # Reset count
-    #     self.positive = 0
-    #     self.negative = 0
-    #
-    #     for element in self.training:
-    #         if element.is_negative() and count < positives:
-    #             count += 1
-    #             temp_dataset.append(element)
-    #             self.negative += 1
-    #         elif element.is_positive():
-    #             temp_dataset.append(element)
-    #             self.positive += 1
-    #         else:
-    #             continue
-    #     self.training = temp_dataset
+        validation_positives = self.get_positives(self.validation)
+        validation_negatives = self.get_negatives(self.validation)
+        validation_frac = 1 - (validation_positives / validation_negatives)
+        self.validation = self.validation.drop(self.validation.query('label == 0')
+                                               .sample(frac=validation_frac, random_state=42).index)
