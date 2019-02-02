1ab86a5be4064435b7d311abc74666a63d825a98
Stefano Frazzetto <s.frazzetto22@gmail.com>
2019-02-02 03:35:16 +0000

    Delete FeatureExtraction

--- PreProcessing/__init__.py
+++ PreProcessing/__init__.py
@@ -1,11 +1,7 @@
 from .CorpusParser import CorpusParser, CorpusName
 from .NLTKStemmer import Stemmer
-# from .CorpusReader import CorpusReader
-from .FeatureExtraction import CountVectorizer
 
 __all__ = [
-    # 'CorpusReader',
-    'CountVectorizer',
     'CorpusParser',
     'CorpusName',
     'Stemmer',
