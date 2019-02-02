751cbd28dcbdfb509e4c3334afed2da5c6ed6994
Stefano Frazzetto <s.frazzetto22@gmail.com>
2019-02-02 03:36:52 +0000

    Implement class for grid-searching parameters

---
+++ Classification/GridSearch.py
@@ -0,0 +1,77 @@
+from pprint import pprint
+from time import time
+
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+
+from Classification import Classifier, ClassifierType
+from Utils import DataStructures, Log
+
+
+class GridSearch(object):
+    """
+    Exhaustive search over specified parameter values for an estimator.
+    https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html
+    """
+
+    classifier: Classifier
+
+    def __init__(self, classifier_type: ClassifierType):
+        self.classifier = Classifier.factory(classifier_type)
+        self.parameters = self.get_parameters()
+
+    def _get_classifier_params(self):
+        params = {}
+        for key, value in self.classifier.get_search_params().items():
+            param_key = f"{self.classifier.get_short_name()}__{key}"
+            params[param_key] = value
+
+        return params
+
+    @staticmethod
+    def _get_count_vectorizer_params():
+        return {
+            'vect__max_df': (0.5, 0.75, 1.0),
+            # 'vect__max_features': (None, 5000, 10000, 50000),
+            'vect__ngram_range': ((1, 1), (1, 2), (2, 2)),  # unigrams or bigrams
+        }
+
+    @staticmethod
+    def _get_tfidf_transformer_params():
+        return {
+            'tfidf__use_idf': [True],
+            # 'tfidf__use_idf': (True, False),
+            # 'tfidf__norm': ('l1', 'l2'),
+        }
+
+    def get_parameters(self):
+        vectorizer_parameters = self._get_count_vectorizer_params()
+        transformer_params = self._get_tfidf_transformer_params()
+        classifier_params = self._get_classifier_params()
+
+        self.parameters = DataStructures.merge_dicts(vectorizer_parameters, transformer_params, classifier_params)
+        return self.parameters
+
+    def fit(self, training_data, training_labels):
+        pipeline = Pipeline([
+            ('vect', CountVectorizer()),
+            ('tfidf', TfidfTransformer()),
+            (self.classifier.get_short_name(), self.classifier.classifier),
+        ])
+
+        grid_search = GridSearchCV(pipeline, self.parameters, cv=5, n_jobs=-1, verbose=1)
+
+        Log.info(f"Performing grid search on {self.classifier.get_name()}...", header=True)
+        Log.info("pipeline:", [name for name, _ in pipeline.steps])
+        Log.info("parameters:")
+        pprint(self.parameters)
+        t0 = time()
+        grid_search.fit(training_data, training_labels)
+        Log.info("done in %0.3fs" % (time() - t0))
+
+        Log.info("Best score: %0.3f" % grid_search.best_score_, header=True)
+        Log.info("Best parameters set:")
+        best_parameters = grid_search.best_estimator_.get_params()
+        for param_name in sorted(self.parameters.keys()):
+            Log.info("\t%s: %r" % (param_name, best_parameters[param_name]))
---
+++ grid_search.py
@@ -0,0 +1,54 @@
+"""
+Author:
+Stefano Frazzetto
+BSc (Hons) Applied Computing
+
+Faculty of Natural Sciences
+Department of Computing Science and Mathematics
+University of Stirling
+"""
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+from Classification import ClassifierType
+from Classification.GridSearch import GridSearch
+from Data import Dataset
+from PreProcessing import CorpusName, CorpusParser
+
+base_path = "/home/stefano/Documents/University/DissertationDatasets"
+pan12_dir = f"{base_path}/pan12-sexual-predator-identification-test-corpus-2012-05-21"
+
+parser = CorpusParser.factory(CorpusName.PAN12, merge_messages=False)
+parser.set_source_directory(pan12_dir)
+dataset = Dataset(parser.__hash__())
+
+# If the dataset for this corpus is already serialized
+if dataset.is_serialized():
+    dataset = dataset.deserialize()
+    dataset.log_info()
+
+# otherwise create it using the parser
+else:
+    if parser.is_serialized():
+        parser = parser.deserialize()
+    else:
+        parser.parse()
+        parser.serialize()
+
+    parser.log_info()
+
+    dataset = parser.get_dataset()
+    dataset.finalize()
+    dataset.log_info()
+    dataset.balance_negatives()
+    dataset.log_info()
+    dataset.serialize()
+
+training_data = dataset.training['text']
+training_labels = dataset.training['label']
+
+testing_data = dataset.validation['text']
+testing_labels = dataset.validation['label']
+
+grid_search = GridSearch(ClassifierType.MultiLayerPerceptron)
+grid_search.fit(training_data, training_labels)
