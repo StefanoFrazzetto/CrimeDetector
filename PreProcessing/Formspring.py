07e8bb7c5b9fa163863803274c2f27d2dc7fb119
Stefano Frazzetto <s.frazzetto22@gmail.com>
2019-02-20 17:24:08 +0000

    Create base classes

--- PreProcessing/Formspring.py
+++ PreProcessing/Formspring.py
@@ -1,14 +1,43 @@
+from enum import Enum
+
+import pandas as pd
+
 from Data import Dataset
 from PreProcessing import CorpusParser
 
 
+class User(object):
+    def __init__(self, user_id: str):
+        self.user_id = user_id
+
+
+class PostLabel(Enum):
+    NEGATIVE = 0
+    POSITIVE = 1
+
+
+class Post(object):
+    def __init__(self):
+        self.user = None
+        self.asker = None
+
+        self.post = ""
+        self.question = ""
+        self.answer = ""
+
+
 class FormspringParser(CorpusParser):
+    raw: pd.DataFrame
 
     def __init__(self):
         super(FormspringParser, self).__init__()
+        self.raw = None
 
     def parse(self):
-        pass
+        self.raw = pd.read_csv(
+            filepath_or_buffer=self.source_path,
+            sep='\t'
+        )
 
     def dump(self, directory, *args):
         pass
@@ -18,3 +47,6 @@ class FormspringParser(CorpusParser):
 
     def log_info(self):
         pass
+
+    def _get_bully3(self):
+        return self.raw.query('not bully3.isnull() and bully3 != "None"')
