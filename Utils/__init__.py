b94c964cf6c593a7e3f3feed4ebbd8ce50d52e78
Stefano Frazzetto <s.frazzetto22@gmail.com>
2019-03-13 13:48:03 +0000

    Fix imports order

--- Utils/__init__.py
+++ Utils/__init__.py
@@ -3,10 +3,10 @@ Utils module containing different utilities to make my life easier.
 """
 
 from .Assert import Assert
+from .Data import DataStructures, Email, Text, Time, Hashing, Numbers
 from .Debug import Debug
 from .File import File
 from .Log import Log
-from .Data import DataStructures, Email, Text, Time, Hashing, Numbers
 from .Plot import Plot
 
 __all__ = [
