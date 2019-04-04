77473a2c330625910b509e70fd973e30a6a37f99
Stefano Frazzetto <s.frazzetto22@gmail.com>
2019-04-04 23:07:53 +0100

    Remove non-existing module

--- Utils/__init__.py
+++ Utils/__init__.py
@@ -4,7 +4,6 @@ Utils module containing different utilities to make my life easier.
 
 from .Assert import Assert
 from .Data import DataStructures, Email, Text, Time, Hashing, Numbers
-from .Debug import Debug
 from .File import File
 from .Log import Log
 from .Plot import Plot
@@ -12,7 +11,6 @@ from .Plot import Plot
 __all__ = [
     'Assert',
     'DataStructures',
-    'Debug',
     'Email',
     'File',
     'Log',
