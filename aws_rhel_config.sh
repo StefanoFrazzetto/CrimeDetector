95134f7dff9d1e883721b1a7e3738e4407ca6363
Stefano Frazzetto <s.frazzetto22@gmail.com>
2019-04-18 11:57:47 +0100

    Add missing repo cloning

--- aws_rhel_config.sh
+++ aws_rhel_config.sh
@@ -18,6 +18,10 @@ yum install git -y
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
 yum install git-lfs -y
 
+# Clone repo
+git clone https://github.com/StefanoFrazzetto/CrimeDetector
+cd CrimeDetector
+
 # Enable LFS and pull
 git lfs enable
 git lfs pull
